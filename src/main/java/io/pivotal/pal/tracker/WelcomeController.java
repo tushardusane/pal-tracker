541d25b792b18ac230bc8d6cd2050b78846978a5
Ubuntu <ubuntu@ip-172-31-19-115.ap-southeast-1.compute.internal>
2020-12-08 07:19:01 +0000

    config assignment

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,22 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+  /*  @Value("${welcome.message}") */
+    private String message;
+
+    public WelcomeController(@Value("${welcome.message}") String message){
+        this.message = message;
+    }
+
+    @GetMapping(value = "/")
+    public String  sayHello() {
+        return message;
+    }
+
+
+}
